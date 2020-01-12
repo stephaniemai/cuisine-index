192a3b3a339b99fcda9b9db75ec77dfce21ca782
stephaniemai <stephaniemai.jacques@gmail.com>
2020-01-12 09:56:31 +0100

    init commit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,45 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: %i[show edit update destroy]
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+    redirect_to recipe_path(@recipe)
+  end
+
+  def edit
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :origin, :category, :cooked, :note, :comment)
+  end
 end
---
+++ todo.md
@@ -0,0 +1,19 @@
+===FRONT===
+
+==Recipes / Index==
+- Liste déroulante pour sélection de la *Source* + comportement conditionnel.
+Si Livre : localisation (nouvelle liste de choix), auteur, titre, éditeur
+Si Dossier : nom (liste des catégories, niveau 1)
+- Liste déroulante pour sélection de la *Catégorie* (liste voir GG sheet). 1 seul choix sélectionnable
+- Afficher les 10 dernières recettes saisies
+
+==Recipes / Show==
+- faire page ou modal?
+
+==Recipes / New==
+- faire formulaire
+
+===FEATURES===
+- Rechercher une recette
+- Ajouter une recette
+- Modifier une recette
